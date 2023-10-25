f9d2e6a7932d1a283b02a66bda5ec66e39527736
Piotr Bobiński <piotr.bobinski96@gmail.com>
2023-10-25 17:48:04 +0200

    pb: |fix| asynchronus operations in swift

--- ios/Classes/AppBadgerPlugin.swift
+++ ios/Classes/AppBadgerPlugin.swift
@@ -2,60 +2,63 @@ import Flutter
 import UIKit
 
 public class AppBadgerPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "app_badger", binaryMessenger: registrar.messenger())
-    let instance = AppBadgerPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  private func areNotificationsEnabled() -> Bool {
-    var result = false;
-     UNUserNotificationCenter.current().getNotificationSettings { (settings) in 
-         if settings.authorizationStatus == .authorized {
-            result = true;
-         }
-         if #available(iOS 12.0, *) {
-            if settings.authorizationStatus == .provisional {
-                 result = true;
-            }
-         }
-         if #available(iOS 14.0, *) {
-             if settings.authorizationStatus == .ephemeral {
-                 result = true;
-             }
-         }
-     }
-    return result;
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "app_badger", binaryMessenger: registrar.messenger())
+        let instance = AppBadgerPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
     
     private func updateBadgeCount(count: Int) {
-    if #available(iOS 16.0, *) {
-        UNUserNotificationCenter.current().setBadgeCount(count);
-    } else {
-        UIApplication.shared.applicationIconBadgeNumber = count;
+        if #available(iOS 16.0, *) {
+            UNUserNotificationCenter.current().setBadgeCount(count);
+        } else {
+            UIApplication.shared.applicationIconBadgeNumber = count;
+        }
     }
-}
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    if(!areNotificationsEnabled()) {
-        // Dont proceed further is notifications are disabled
-        result(nil);
-        return;
+    
+    private func proceedWithBadge(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "updateCount":
+            let arguments = call.arguments as! Dictionary<String,Int>;
+            let count = arguments["count"]!;
+            updateBadgeCount(count: count);
+            result(nil);
+        case "remove":
+            updateBadgeCount(count: 0);
+            result(nil);
+        case "isSupported":
+            result(true); // iOS Always supports updating badge
+        default:
+            result(FlutterMethodNotImplemented)
+        }
     }
-
-    switch call.method {
-    case "updateCount":
-        let arguments = call.arguments as! Dictionary<String,Int>;
-        let count = arguments["count"]!;
-        updateBadgeCount(count: count);
-        result(nil);
-    case "remove": 
-        updateBadgeCount(count: 0);
-        result(nil);
-    case "isSupported": 
-        result(true); // iOS Always supports updating badge
-    default:
-        result(FlutterMethodNotImplemented)
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        UNUserNotificationCenter.current().getNotificationSettings { (settings) in
+            if settings.authorizationStatus == .denied {
+                result(nil);
+                return;
+            }
+            if settings.authorizationStatus == .notDetermined {
+                result(nil);
+                return;
+            }
+            if settings.authorizationStatus == .authorized {
+                self.proceedWithBadge(call: call, result: result);
+                return;
+            }
+            if #available(iOS 12.0, *) {
+                if settings.authorizationStatus == .provisional {
+                    self.proceedWithBadge(call: call, result: result);
+                    return;
+                }
+            }
+            if #available(iOS 14.0, *) {
+                if settings.authorizationStatus == .ephemeral {
+                    self.proceedWithBadge(call: call, result: result);
+                    return;
+                }
+            }
+        }
     }
-  }
 }
